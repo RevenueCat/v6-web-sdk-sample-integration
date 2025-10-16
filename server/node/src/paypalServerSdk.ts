d0af347ab88b8e36fc8c492377897b6d83ed9590
vicfergar <victor@revenuecat.com>
2025-10-17 00:51:43 +0200

    add plan name

--- server/node/src/paypalServerSdk.ts
+++ server/node/src/paypalServerSdk.ts
@@ -239,6 +239,7 @@ export async function createSetupTokenWithSampleDataForPayPal() {
       paypal: {
         usagePattern: UsagePattern.SubscriptionPrepaid,
         billingPlan: {
+          name: "Basic",
           billingCycles: [
             {
               tenureType: TenureType.Trial,
